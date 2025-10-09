cfed8d0719f145293a6cb24eafd8b3998ab9fcc0
jkbk2004 <jong.kim@noaa.gov>
2025-10-09 17:23:11 -0500

    update template

--- tests-dev/test_loader.py
+++ tests-dev/test_loader.py
@@ -20,8 +20,7 @@ class TestLoader:
                 app_yaml = yaml.safe_load(f)
 
             for build_id, block in app_yaml.items():
-                app_prefix = build_id.split("_")[0]
-                if app_prefix not in self.selected_apps:
+                if not any(build_id.startswith(app) for app in self.selected_apps):
                     continue
 
                 build_info = block.get("build", {})
@@ -29,7 +28,6 @@ class TestLoader:
                 option = build_info.get("option", "")
                 turnoff = build_info.get("turnoff", [])
 
-                # Compile task
                 self.tests.append({
                     "id": build_id,
                     "compiler": compiler,
@@ -37,9 +35,10 @@ class TestLoader:
                     "type": "compile"
                 })
 
-                # Run tasks
                 for test_entry in block.get("tests", []):
                     for test_id, test_meta in test_entry.items():
+                        if test_id in turnoff:
+                            continue
                         self.tests.append({
                             "id": test_id,
                             "compiler": compiler,
