33e9e1e6f2bfdc48fa0cab7b35ebc167fc7a6dad
JONG KIM <jong.kim@noaa.gov>
2025-10-18 10:59:35 -0400

    Update extract_resources.sh

--- tests-dev/extract_resources.sh
+++ tests-dev/extract_resources.sh
@@ -5,6 +5,8 @@ MACHINE_ID=$2
 export RTVERBOSE=0
 
 source "${PATHRT}/default_vars.sh"
+source "${PATHRT}/rt_utils.sh
+source "${PATHRT}"/tests/"${TEST_NAME}"
 
 TEST_SCRIPT="${PATHRT}/tests/${TEST_NAME}"
 if [[ ! -f "${TEST_SCRIPT}" ]]; then
@@ -13,9 +15,22 @@ if [[ ! -f "${TEST_SCRIPT}" ]]; then
 fi
 
 source "${TEST_SCRIPT}"
-source "${PATHRT}/ufs_test_utils.sh"
+if [[ ${ESMF_THREADING} == true ]]; then
+    compute_petbounds_and_tasks_esmf_threading
+else
+    compute_petbounds_and_tasks_traditional_threading
+fi
 
-set_run_task "${TEST_NAME}" "${MACHINE_ID}"
+TPN=$(( TPN / THRD ))
+NODES=$(( TASKS / TPN ))
+if (( NODES * TPN < TASKS )); then
+	 NODES=$(( NODES + 1 ))
+fi
+PPN=$(( TASKS / NODES ))
+if (( TASKS - ( PPN * NODES ) > 0 )); then
+   PPN=$((PPN + 1))
+fi
+export WLCLK
 
 # Emit clean key-value pairs for YAML parsing
 echo "ppn: ${TPN}"
