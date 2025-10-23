385464ced23fa7f07ed5ba3f779fdd9970dbca32
JONG KIM <jong.kim@noaa.gov>
2025-10-23 16:29:48 -0400

    Update run.sh

--- tests-dev/run.sh
+++ tests-dev/run.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+export EXP_PID=$$
+
+pid = os.environ.get("EXP_PID", str(os.getpid()))
+rundir_root = f"{machine_config['RUNDIR_PATH']}/rt_{pid}"
+
 python build_rocotoxml.py --machine orion --manifest app_manifest.yaml --output rocoto_orion.xml --yamls_dir tests-yamls/configs/by_app
