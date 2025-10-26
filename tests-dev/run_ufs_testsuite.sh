7eefc89ecf7a0dc6eb3974b897892354a8bde14a
JONG KIM <jong.kim@noaa.gov>
2025-10-26 13:15:43 -0400

    Update run_ufs_testsuite.sh

--- tests-dev/run_ufs_testsuite.sh
+++ tests-dev/run_ufs_testsuite.sh
@@ -40,8 +40,8 @@ done
 if $LINK_TESTS; then
   if [[ ! -e "rt.sh" || ! -d "parm" || ! -d "scripts" ]]; then
     echo "[INFO] Copying test files from ../tests/ to current directory"
-    cp -r ../tests/rt.conf .
-    cp -r ../tests/bl_date.conf .
+    cp ../tests/rt.conf .
+    cp ../tests/bl_date.conf .
     cp -r ../tests/fv3_conf .
     cp -r ../tests/parm .
     cp -r ../tests/tests .
